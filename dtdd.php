8c3022364c79be2ec134c57a0cc16c5f16d9fcc2
Linh Dang <mlinh211@gmail.com>
2015-11-22 15:24:40 +0700

    log in

--- dtdd.php
+++ dtdd.php
@@ -18,50 +18,94 @@
 
 </head>
 <body>
-<a href="#" class="back-to-top">Back to Top</a>
+	<a href="#" class="back-to-top">Back to Top</a>
 	<div class="container">
 		<div id="header"></div>
 		<!--------- HEADER----------------->
 		<div class="wrapbread">
 			<div class="rowtop">
 				<div class="clr"></div>
-				<h1>Điện thoại OPPO Neo 7</h1>
+				<h1>Điện thoại Samsung Galaxy J5 J500 White</h1>
 				<span>Hơn 3.800 lượt mua tuần qua</span>
 				<div class="clr"></div>
 			</div>
-			
 		</div>
 		<section class="">
 		<div class="boxright">
 			<aside class="picture "> <img alt="Samsung Galaxy S6 Edge Plus"
 				src="images/samsung-galaxy-s6-edge-plus-400x533.png" height="533"
-				width="400"></aside>
-			<aside class="price_sale"> <strong>19.990.000₫ <span
-					class="localprice"></span>
-			</strong> <label class="infopromotion"><b>Quà khuyến mãi</b></label>
-			<ul class="infocomes">
-				<li><i class="icondetail-check"></i>Bộ sản phẩm gồm: <a
-					class="stdImg">Hộp, Sạc, Tai nghe, Sách hướng dẫn, Cáp, Cây lấy sim
-				</a></li>
-				<li><i class="icondetail-check"></i>Bảo hành chính hãng: thân máy 12
-					tháng, pin 6 tháng, sạc 6 tháng</li>
-				<li><i class="icondetail-check"></i>Giao hàng tận nơi miễn phí trong
-					<strong>30 phút</strong>. <a href="#" target="blank">Tìm hiểu</a></li>
-				<li><i class="icondetail-check"></i>Đổi trả sản phẩm lỗi miễn phí
-					trong <strong>14 ngày</strong>. <a href="#" target="blank">Tìm hiểu</a></li>
-			</ul>
-			<a href="#" class="buynow">Thêm vào giỏ hàng </a>
-			<div class="buycall">
-				Gọi đặt mua <b>1800.xxxx</b> hoặc <b>(08)12234456</b>
-				<span>(7h30 - 22h)</span>
+				width="300">
+				<ul class="ls p-select-color">
+					<li><a class="active" href="javascript:;" data-toggle="modal" data-target="#image-modal"
+						title=" Điện thoại Samsung Galaxy J5 J500 White"> <img
+							src="http://img.mediamart.vn/Product/tiny_27077_17913_dien-thoai-samsung-galaxy-j5.jpg"
+							alt=" Điện thoại Samsung Galaxy J5 J500 White"
+							title=" Điện thoại Samsung Galaxy J5 J500 White"><span></span></a>
+						
+						<!-- Modal -->
+						<div class="modal fade" id="image-modal" tabindex="-1" role="dialog"
+							aria-labelledby="myModalLabel">
+							<div class="modal-dialog" role="document">
+								<div class="modal-content">
+									<div class="modal-header" >
+										<button type="button" class="close" data-dismiss="modal"
+											aria-label="Close">
+											<span aria-hidden="true">&times;</span>
+										</button>
+										<h4 class="modal-title" id="myModalLabel">Samsung Galaxy S6 Edge Plus</h4>
+									</div>
+									<div class="modal-body">
+									<img alt="Samsung Galaxy S6 Edge Plus"
+											src="images/samsung-galaxy-s6-edge-plus-400x533.png" >
+									</div>
+									
+								</div>
+							</div>
+						</div></li>
+				</ul></aside>
+			<div class="wrap-right-pd">
+				<div class="pro-detail">
+					<div class="p-model">
+						<strong>3.990.000₫ </strong>
+						<span>Mã hàng: 506462</span>
+						|
+						<span style="color: #ff8a00; font: bold 12px/27px Arial;">Còn hàng</span>
+					</div>
+					<div class="border-line"></div>
+					<div class="p-spec" itemprop="description">
+						<p>
+							- Bộ xử lý: 4 nhân 1.2GHz, Qualcomm MSM8916 Snapdragon 410<br> -
+								Màn hình: 5.0 inch, HD 720 x 1280 pixels<br> - Bộ nhớ: 8GB, RAM:
+									1,5GB<br> - Thẻ nhớ ngoài lên đến 64GB<br> - Camera chính:
+											13MP, 1080p @30fps/ Camera phụ: 5MP<br> - Dual-Sim, 4G LTE,
+												Wifi, bluetooth, GPS<br> - Hệ điều hành: Android 5.1
+													Lollipop<br> - Pin: 2600mAh 
+						
+						</p>
+						<div class="clr"></div>
+					</div>
+					<div class="clr"></div>
+					<div class="border-line"></div>
+					<ul class="ls p-program">
+						<li><i class="icondetail-check"></i>Mua phụ kiện, điện thoại di
+							động, máy tính bảng, Đổi mới miễn phí trong 30 ngày. <span>
+								<a>Xem chi tiết</a>
+							</span>.</li>
+						<li><i class="icondetail-check"></i>Chuyên trang Ưu đãi trả góp
+							0%, Tặng quà trị giá đến 590.000đ với, ANZ, ACS, PPF, FECredit..
+						</li>
+						<li><i class="icondetail-check"></i> Chính sách vận chuyển miễn
+							phí toàn miền Bắc đến 200km. Chi tiết xem <span>tại đây</span>.</li>
+					</ul>
+					<div class="clr"></div>
+					<a href="/dtdd/oppo-neo-7/dat-hang" class="buynow">Mua ngay<span>Giao
+							tận nơi hoặc nhận ở cửa hàng</span></a>
+					<div class="clr"></div>
+				</div>
 			</div>
-			
-			</aside>
 			<div class="clr"></div>
 		</div>
 		</section>
 	</div>
-	
-	
 </body>
 </html>
--- nav-bar.php
+++ nav-bar.php
@@ -1,29 +1,94 @@
 <?php
-session_start();
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+session_start ();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <link rel="stylesheet" type="text/css" href="css/m-head.css">
-<body>
-	<header>
+	<body>
+		<header>
 		<div class="wrap-main">
-			<form id="search-site" action="/tim-kiem" method="get" onsubmit="return submitSearchForm();"
-				autocomplete="off">
-				<input class="topinput" id="search-keyword" name="search-keyword" placeholder="Bạn tìm gì..."
-					autocomplete="off" value="" type="text">
+			<form id="search-site" action="/tim-kiem" method="get"
+				onsubmit="return submitSearchForm();" autocomplete="off">
+				<input class="topinput" id="search-keyword" name="search-keyword"
+					placeholder="Bạn tìm gì..." autocomplete="off" value="" type="text">
 					<button class="btntop" type="submit">
 						<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
 					</button>
 			
 			</form>
-			<nav> <a class="mobile" href="/didong/main-p"><i class="icontgdd-mobile"></i>Điện thoại</a> <a
-				href="/may-tinh-bang" class="tablet"><i class="icontgdd-tablet"></i>Tablet</a> <a href="/laptop"
-				class="laptop"><i class="icontgdd-laptop"></i>Laptop</a> <a href="/hoi-dap" class="ask"><i
-				class="icontgdd-ask"></i>Hỏi đáp</a> <a href="/khuyen-mai" class="promo"><i
-				class="icontgdd-promo"></i>Khuyến mãi</a> </nav>
+			<nav> <a class="mobile" href="/didong/main-p"><i
+				class="icontgdd-mobile"></i>Điện thoại</a> <a href="/may-tinh-bang"
+				class="tablet"><i class="icontgdd-tablet"></i>Tablet</a> <a
+				href="/laptop" class="laptop"><i class="icontgdd-laptop"></i>Laptop</a>
+			<a href="/hoi-dap" class="ask"><i class="icontgdd-ask"></i>Hỏi đáp</a>
+			<a href="/khuyen-mai" class="promo"> <i class="icontgdd-promo"></i>Khuyến
+				mãi
+			</a> <a href="" data-toggle="modal" data-target="#log-in"
+				class="login"> <img src="images/settings-512.png" height="24"
+				class="icon-user" width="25"></img>Đăng nhập
+			</a> <!-- Button trigger modal --> <!-- Modal -->
+			<div class="modal fade in" id="log-in" tabindex="-1" role="dialog"
+				aria-labelledby="Đăng nhập/ Đăng ký">
+				<div class="modal-dialog" role="document" id="container">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal"
+								aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+							<h4 class="modal-title popup-title" id="myModalLabel">Đăng nhập/
+								Đăng ký</h4>
+						</div>
+						<div class="modal-body " id="">
+							<div id="emailLogin" class="left">
+								<form id="form_login1" method="post"
+									action="/dynamic/client_login/mediafire.php"
+									target="widgetwork">
+									<label class="ieLabel">Email address</label> <input
+										pattern="^(.)+@[A-Za-z0-9]([A-Za-z0-9\.-]*[A-Za-z0-9])?\.[A-Za-z]{1,13}$"
+										name="login_email" id="widget_login_email"
+										placeholder="Email address" type="email"> <label
+										class="ieLabel">Password</label> <input name="login_pass"
+										id="widget_login_pass" placeholder="Password" maxlength="30"
+										type="password">
+											<div id="login_remember_wrap">
+												<input name="login_remember" id="login_remember"
+													checked="checked" type="checkbox"> <label
+													for="login_remember">Keep me logged in</label>
+											
+											</div>
+											<button type="submit" class="buynow" onclick="">Đăng nhập</button>
+								
+								</form>
+							</div>
+							<div id="emailLogin" class="right">
+								<form id="form_login1" method="post"
+									action="/dynamic/client_login/mediafire.php"
+									target="widgetwork">
+									<label class="ieLabel">Email address</label> <input
+										pattern="^(.)+@[A-Za-z0-9]([A-Za-z0-9\.-]*[A-Za-z0-9])?\.[A-Za-z]{1,13}$"
+										name="login_email" id="widget_login_email"
+										placeholder="Email address" type="email"> <label
+										class="ieLabel">Password</label> <input name="login_pass"
+										id="widget_login_pass" placeholder="Password" maxlength="30"
+										type="password"> <label class="ieLabel">Password</label> <input
+											name="login_pass" id="widget_login_pass"
+											placeholder="Password" maxlength="30" type="password">
+												<button type="submit" class="buynow" onclick="">Đăng ký</button>
+								
+								</form>
+							</div>
+							<div class="clr"></div>
+						</div>
+					</div>
+				</div>
+			
+			</nav>
 		</div>
-	</header>
-	<script type="text/javascript">
+		</header>
+		<script type="text/javascript">
 	$('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
-</script>
-</body>
+ 	</script>
+	</body>
+
 </html>
